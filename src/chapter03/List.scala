68152108a9ca4bc2460e86d22d8573a43eb08c83
mjpark03 <mjpark03@gmail.com>
2016-12-31 19:46:27 +0900

    exercise 3.1 & 3.2

--- src/chapter03/List.scala
+++ src/chapter03/List.scala
@@ -22,12 +22,23 @@ object List {
     if(as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  /*
+    3.1
+  */
   val x = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
     case Cons(h, t) => h + sum(t)
     case _ => 101
+  }
+
+  /*
+    3.2
+  */
+  def tail[A](list: List[A]): List[A] = list match {
+    case Nil => Nil
+    case Cons(_, tail) => tail
   }
 
 }
