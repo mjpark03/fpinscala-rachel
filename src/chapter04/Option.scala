797ef0c7327f381c255442641ddec5b0642f2805
mjpark03 <mjpark03@gmail.com>
2017-01-09 13:04:58 +0900

    exercise 4.1

--- src/chapter04/Option.scala
+++ src/chapter04/Option.scala
@@ -1,11 +1,21 @@
 
 
-sealed trait Option[+A] 
+sealed trait Option[+A] {
+  
+  /*
+    exercise 4.1
+  */
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(v) => Some(f(v))
+  }
+}
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  
   def mean(seq: Seq[Double]): Option[Double] = {
     if(seq.isEmpty) None
     else Some(seq.sum / seq.length)
