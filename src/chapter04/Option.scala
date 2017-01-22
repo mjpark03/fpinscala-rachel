23e323cce7e8dfeb290aeb5b5accbfcca22b041a
mjpark03 <mjpark03@gmail.com>
2017-01-22 21:45:58 +0900

    exercise 4

--- src/chapter04/Option.scala
+++ src/chapter04/Option.scala
@@ -56,4 +56,11 @@ object Option {
     a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
   }
 
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a match {
+      case Nil => Some(Nil)
+      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
+  }
+
 }
